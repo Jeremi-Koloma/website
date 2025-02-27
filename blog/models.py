4b15aeb99d83119641f03e04afa189a6df51bd09
Jeremi-Koloma <kolomajeremi60@gmail.com>
2025-02-27 22:09:38 +0000

    creation des migrations

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,4 @@ class BlogPost(models.Model):
     published = models.BooleanField(default = False) # Par défaut l'article n'est pas publier
     date = models.DateField(blank = True) # Ce champ peut-être vide
     content = models.TextField()
+    description = models.TextField()

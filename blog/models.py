78129d80ee4d532868073f9d064ecf1a6ab16a53
Jeremi-Koloma <kolomajeremi60@gmail.com>
2025-02-27 13:04:13 +0000

    ajout model BlogPost

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length = 100)
+    slug = models.SlugField()
+    published = models.BooleanField(default = False) # Par défaut l'article n'est pas publier
+    date = models.DateField(blank = True) # Ce champ peut-être vide
+    content = models.TextField()

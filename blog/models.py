21578a88160663f178b3693f4c2ae07c0367d402
Jeremi-Koloma <kolomajeremi60@gmail.com>
2025-02-27 23:04:12 +0000

    Enregistrement dans la base de donnée

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,6 @@ class BlogPost(models.Model):
     title = models.CharField(max_length = 100)
     slug = models.SlugField()
     published = models.BooleanField(default = False) # Par défaut l'article n'est pas publier
-    date = models.DateField(blank = True) # Ce champ peut-être vide
+    date = models.DateField(blank = True, null = True) # Ce champ peut-être vide
     content = models.TextField()
     description = models.TextField()

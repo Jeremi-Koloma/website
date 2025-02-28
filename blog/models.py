8ffa33be898cb4abf8ad75e244fa473e4e370f1d
Jeremi-Koloma <kolomajeremi60@gmail.com>
2025-02-28 23:25:49 +0000

    Ajout model Category

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+    slug = models.SlugField()
+
+
 # Create your models here.
 class BlogPost(models.Model):
     title = models.CharField(max_length = 100)

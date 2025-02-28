45395e02c200639e30465597652df9fd02f8fd2c
Jeremi-Koloma <kolomajeremi60@gmail.com>
2025-02-28 23:49:25 +0000

    migrations ManyToMany

--- blog/models.py
+++ blog/models.py
@@ -9,6 +9,7 @@ class Category(models.Model):
 # Create your models here.
 class BlogPost(models.Model):
     author = models.ForeignKey(User, on_delete = models.SET_NULL, null = True) # Clé étrangère
+    category = models.ManyToManyField(Category) # ManyToMany
     title = models.CharField(max_length = 100)
     slug = models.SlugField()
     published = models.BooleanField(default = False) # Par défaut l'article n'est pas publier
